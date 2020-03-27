d697e2777ac08ff677cdf495cef2107b0817c07e
Marijn Kampf <github.com@gwobbel.com>
2020-03-27 11:28:03 +0000

    Added compatibility with PHP 7.2

--- _config.php
+++ _config.php
@@ -1,6 +1,10 @@
 <?php
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 define('ASSETS_GALLERY_BASE', basename(dirname(__FILE__)));
 
-Object::add_extension('Folder', 'AssetsGalleryFolder');
-Object::add_extension('File', 'AssetsGalleryFile');
+SS_Object::add_extension('Folder', 'AssetsGalleryFolder');
+SS_Object::add_extension('File', 'AssetsGalleryFile');
